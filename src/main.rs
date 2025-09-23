b8090da6ea4f592fa011256c466d7682fb85e7f6
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-09-23 22:21:26 +0545

    added: thread handler

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, process, sync::Arc};
+use std::{env, thread, process, sync::Arc};
 
 use riic::Client;
 fn main() {
     let arguments: Arc<[String]> = env::args().collect();
     let build = Client::build(&arguments).unwrap_or_else(|err| {
-        eprint!("noob");
+        eprint!("noob:{}", err);
         process::exit(0);
     });
+
+    let handle = build.initialize_connection();
+    handle.join().unwrap();
 }
